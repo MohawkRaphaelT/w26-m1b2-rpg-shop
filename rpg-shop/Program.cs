27158b2d375ae25a81d780af815e5b3155cfb4a6
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2026-01-22 16:07:59 -0500

    Add project - Implement scenario up to about step 3.5

---
+++ rpg-shop/Program.cs
@@ -0,0 +1,32 @@
+﻿// Step 1: Ask player for name (and remember it)
+Console.WriteLine("Hello there traveller, what is your name?");
+string playerName = Console.ReadLine();
+
+// Step 2: Ask player for sum of held gold (currency)
+Console.WriteLine("How much gold do you have on you, traveller?");
+string heldGold = Console.ReadLine();
+
+// Step 3+: Tell player about sword, do they wish to purchase?
+Console.WriteLine("I have a 'bronze sword' for sale for 15 gold coins.");
+Console.WriteLine("Do you wish to purchase it?");
+string doPurchaseSword = Console.ReadLine();
+if (doPurchaseSword == "yes")
+{
+    // 3a buy sword
+    if (/* has enough gold */)
+    {
+        // deduct cost
+        // inform player of remaining gold
+    }
+    else
+    {
+        // not enough gold
+    }
+}
+else
+{
+    Console.WriteLine($"Farewell on your travels, {playerName}.");
+}
+// Test our variables for our own sanity
+//Console.WriteLine("Name: " + playerName + ", Gold: " + heldGold);
+//Console.WriteLine($"Name: {playerName}, Gold: {heldGold}.");
