33a288913164a56cf7dd7dd169f21c4bbd198775
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2026-01-22 16:43:56 -0500

    Implement transaction

--- rpg-shop/Program.cs
+++ rpg-shop/Program.cs
@@ -3,8 +3,10 @@ Console.WriteLine("Hello there traveller, what is your name?");
 string playerName = Console.ReadLine();
 
 // Step 2: Ask player for sum of held gold (currency)
-Console.WriteLine("How much gold do you have on you, traveller?");
-string heldGold = Console.ReadLine();
+Console.WriteLine("How much gold do you have, traveller?");
+//string heldGoldText = Console.ReadLine();
+//int heldGold = int.Parse(heldGoldText);
+int heldGold = int.Parse(Console.ReadLine());
 
 // Step 3+: Tell player about sword, do they wish to purchase?
 Console.WriteLine("I have a 'bronze sword' for sale for 15 gold coins.");
@@ -13,14 +15,21 @@ string doPurchaseSword = Console.ReadLine();
 if (doPurchaseSword == "yes")
 {
     // 3a buy sword
-    if (/* has enough gold */)
+    if (heldGold >= 15)
     {
         // deduct cost
+        heldGold -= 15;
+        //heldGold = heldGold - 15;
+
         // inform player of remaining gold
+        Console.WriteLine($"You purchased the 'bronze sword'.");
+        Console.WriteLine($"You have {heldGold} gold remaining.");
     }
     else
     {
         // not enough gold
+        Console.WriteLine($"{playerName}, you only have {heldGold} gold!");
+        Console.WriteLine($"You cannot afford the 'bronze sword'!");
     }
 }
 else
@@ -29,4 +38,4 @@ else
 }
 // Test our variables for our own sanity
 //Console.WriteLine("Name: " + playerName + ", Gold: " + heldGold);
-//Console.WriteLine($"Name: {playerName}, Gold: {heldGold}.");
+Console.WriteLine($"Name: {playerName}, Gold: {heldGold}.");
